0e27c47611400fc28b87b64fd07ca42b2f064cc4
Favian <favouriteteo@gmail.com>
2023-01-15 20:04:31 +0800

    refactor: updated Product mocks to match new schema

--- src/util/mocks.ts
+++ src/util/mocks.ts
@@ -7,14 +7,29 @@ export const mockProduct1: Product = {
     name: "Tissue Paper",
     description: "This is a really good roll of toilet paper. Might I even say - the best. It has so much pulpiness, and feels smooth and stable and silky on your skin.",
     price: 13.50,
-    thumbnailUrl: "https://alley.tv/wp-content/uploads/2016/11/lh_0000_Background.jpg",
-    imageUrls: [
-        "https://alley.tv/wp-content/uploads/2016/11/lh_0000_Background.jpg",
-        "https://i.scdn.co/image/ab6761610000e5eb45c0f559e90489af64359a59",
-        "https://i.ytimg.com/vi/wMulm-gnCLs/maxresdefault.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/1/1a/Lisa_Hannigan_10_16_2017_-24_%2837889906544%29.jpg",
+    images: [
+        {
+            productId: "product1",
+            isThumbnail: true,
+            url: "https://alley.tv/wp-content/uploads/2016/11/lh_0000_Background.jpg",
+        },
+        {
+            productId: "product1",
+            isThumbnail: false,
+            url: "https://i.scdn.co/image/ab6761610000e5eb45c0f559e90489af64359a59",
+        },
+        {
+            productId: "product1",
+            isThumbnail: false,
+            url: "https://i.ytimg.com/vi/wMulm-gnCLs/maxresdefault.jpg",
+        },
+        {
+            productId: "product1",
+            isThumbnail: false,
+            url: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Lisa_Hannigan_10_16_2017_-24_%2837889906544%29.jpg",
+        },
     ],
-    quantity: 0,
+    quantity: 6,
     createdAt: new Date("2021-01-01").toISOString(),
     updatedAt: new Date("2021-01-01").toISOString(),
     categories: [
@@ -42,12 +57,27 @@ export const mockProduct2: Product = {
     name: "Bowl",
     description: "This is a nice round bowl for serving food in.",
     price: 5.00,
-    thumbnailUrl: "https://i.ytimg.com/vi/wMulm-gnCLs/maxresdefault.jpg",
-    imageUrls: [
-        "https://i.ytimg.com/vi/wMulm-gnCLs/maxresdefault.jpg",
-        "https://i.scdn.co/image/ab6761610000e5eb45c0f559e90489af64359a59",
-        "https://alley.tv/wp-content/uploads/2016/11/lh_0000_Background.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/1/1a/Lisa_Hannigan_10_16_2017_-24_%2837889906544%29.jpg",
+    images: [
+        {
+            productId: "product2",
+            isThumbnail: false,
+            url: "https://alley.tv/wp-content/uploads/2016/11/lh_0000_Background.jpg",
+        },
+        {
+            productId: "product2",
+            isThumbnail: false,
+            url: "https://i.scdn.co/image/ab6761610000e5eb45c0f559e90489af64359a59",
+        },
+        {
+            productId: "product2",
+            isThumbnail: false,
+            url: "https://i.ytimg.com/vi/wMulm-gnCLs/maxresdefault.jpg",
+        },
+        {
+            productId: "product2",
+            isThumbnail: true,
+            url: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Lisa_Hannigan_10_16_2017_-24_%2837889906544%29.jpg",
+        },
     ],
     quantity: 2,
     createdAt: new Date("2021-01-01").toISOString(),
