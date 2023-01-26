fce80cb1bdb1d4d7a0521592b228a4a2f4e47491
Favian <favouriteteo@gmail.com>
2023-01-27 02:07:05 +0800

    feat: view listed products in account

--- src/pages/account/AccountPage.tsx
+++ src/pages/account/AccountPage.tsx
@@ -1,11 +1,13 @@
 import { Container, Heading, Tab, TabList, TabPanels, Tabs } from "@chakra-ui/react";
 import { Footer } from "../common/Footer";
 import { BackButton } from "../common/BackButton";
-import { FaDollarSign, FaUser } from "react-icons/all";
+import { FaDollarSign, FaPen, FaUser } from "react-icons/all";
 import { FaShoppingCart } from "react-icons/fa";
 import { ProfileTab } from "./components/ProfileTab";
 import { OrdersTab } from "./components/OrdersTab";
 import { SalesTab } from "./components/SalesTab";
+import { DeleteAccountModal } from "./components/DeleteAccountModal";
+import { ListedProductsTab } from "./components/ListedProductsTab";
 
 export const AccountPage: React.FC = () => {
     return <>
@@ -21,16 +23,20 @@ export const AccountPage: React.FC = () => {
             >
                 <TabList gap={"2"} p={"2"} bg={"gray.50"} borderRadius={"12"}>
                     <Tab borderRadius={"10"}><FaUser/>&nbsp; Profile</Tab>
+                    <Tab borderRadius={"10"}><FaPen/>&nbsp; Listed Products</Tab>
                     <Tab borderRadius={"10"}><FaShoppingCart/>&nbsp; Orders</Tab>
                     <Tab borderRadius={"10"}><FaDollarSign/>&nbsp; Sales</Tab>
                 </TabList>
                 <TabPanels mt={"4"} p={"4"} bg={"gray.50"} borderRadius={"6"}>
                     <ProfileTab />
+                    <ListedProductsTab />
                     <OrdersTab />
                     <SalesTab />
                 </TabPanels>
             </Tabs>
         </Container>
         <Footer />
+        <DeleteAccountModal />
+
     </>;
 };
---
+++ src/pages/account/components/ListedProductsTab.tsx
@@ -0,0 +1,79 @@
+import {
+    Button,
+    Card,
+    CardBody,
+    Center,
+    Heading,
+    Hide,
+    HStack,
+    Image,
+    Stack,
+    Stat,
+    StatHelpText,
+    StatNumber,
+    TabPanel,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
+import { useGetUserProductsQuery } from "../../../app/api/products";
+import { useAuth } from "../../../app/context/AuthContext";
+import { Product } from "../../../util/models/Product";
+import { AppRoutes } from "../../../util/routes";
+import { useNavigate } from "react-router-dom";
+
+
+export const ListedProductsTab: React.FC = () => {
+    const { user } = useAuth();
+    const { data: products, isLoading } = useGetUserProductsQuery(user!.id);
+
+    return <TabPanel>
+        <Heading size={"md"} mb={"8"}>Your Listed Products</Heading>
+        {products &&
+            (products.length === 0
+                ?
+                <Center my={"12"} w={"full"} h={"48"} borderWidth={"medium"} borderStyle={"dashed"} borderRadius={"10"}>
+                    <Heading size={"sm"}>You haven&apos;t listed any products!</Heading>
+                </Center>
+                :
+                <VStack spacing={"4"}>
+                    {products.map(p => <ProductCard key={`product-card-${p.id}`} product={p} />)}
+                </VStack>
+            )
+        }
+    </TabPanel>;
+};
+
+const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
+    const navigate = useNavigate();
+
+    return <Card
+        w={"full"}
+        direction={"row"}
+        variant={"outline"}
+        borderRadius={"lg"}
+        borderColor={"gray.200"}
+        overflow={"hidden"}
+        _hover={{
+            borderColor: "gray.400",
+        }}
+        bg={"white"}
+        transition={"border 0.2s ease-out"}
+    >
+        <Image fit={"cover"} boxSize={"48"} src={product.images.find(i => i.isThumbnail)!.url} />
+        <CardBody px={"8"} display={"flex"} gap={"8"} justifyContent={"space-between"} alignItems={"center"}>
+            <Stack mt={"2"} spacing={"3"}>
+                <HStack align={"center"} spacing={"2"}>
+                    <Heading size={{ base: "sm", md: "md" }}>{product.name}</Heading>
+                </HStack>
+                <Hide below={"lg"}>
+                    <Text>{product.description}</Text>
+                </Hide>
+                <Stat mt={"4"}>
+                    <StatNumber>${product.price.toFixed(2)}</StatNumber>
+                    <StatHelpText>Stock: {product.quantity}</StatHelpText>
+                </Stat>
+            </Stack>
+            <Button size={{ md: "sm", lg: "md" }} onClick={() => navigate(AppRoutes.Product(product.id))}>View Product</Button>
+        </CardBody>
+    </Card>;
+};
