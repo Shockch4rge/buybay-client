a8f9941955224fcb66aff9bf82ed7d747098c8e5
Favian <favouriteteo@gmail.com>
2023-01-30 22:39:34 +0800

    refactor: add horizontal padding

--- src/pages/common/EmptyContent.tsx
+++ src/pages/common/EmptyContent.tsx
@@ -5,10 +5,12 @@ export const EmptyContent: React.FC<PropsWithChildren<BoxProps>> = props => {
     return <Center
         w={"full"}
         py={"12"}
+        px={"4"}
         borderRadius={"md"}
         borderWidth={"medium"}
         borderStyle={"dashed"}
         borderColor={"gray.300"}
+        textAlign={"center"}
         {...props}
     >
         {props.children}
