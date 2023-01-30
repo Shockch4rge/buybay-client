afdaf44f442df24b313d5f70abb272bb0e7eada6
Favian <favouriteteo@gmail.com>
2023-01-30 22:41:10 +0800

    fix/feat: added more links

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,9 +1,16 @@
-import { Button, Container, Heading, HStack, Text, VStack } from "@chakra-ui/react";
-import { ProductCard } from "./components/ProductCard";
 import { FaArrowRight, FaShoppingCart } from "react-icons/fa";
-import { Footer } from "../common/Footer";
-import { Product } from "../../util/models/Product";
+import { useNavigate } from "react-router-dom";
+
+import { Button, Container, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+
 import { useAppSelector } from "../../app/store/hooks";
+import { Product } from "../../util/models/Product";
+import { AppRoutes } from "../../util/routes";
+import { EmptyContent } from "../common/EmptyContent";
+import { Footer } from "../common/Footer";
+import { ProductCard } from "./components/ProductCard";
+import { NavBar } from "../common/NavBar";
+import { BackButton } from "../common/BackButton";
 
 const collapseDuplicates = (products: Product[]) => {
     const map = new Map<string, { product: Product; count: number }>();
@@ -24,10 +31,13 @@ const collapseDuplicates = (products: Product[]) => {
 };
 
 export const CartPage: React.FC = () => {
+    const navigate = useNavigate();
     const items = useAppSelector(state => state.cart.items);
 
     return <>
+        <NavBar />
         <Container maxW={{ md: "3xl", lg: "5xl", xl: "6xl" }}>
+            <BackButton />
             <VStack my={"12"} align={"start"} spacing={"4"}>
                 <HStack spacing={"4"}>
                     <FaShoppingCart size={"1.8em"} />
@@ -39,12 +49,12 @@ export const CartPage: React.FC = () => {
             </VStack>
             <VStack spacing={"4"} align={"stretch"}>
                 {items.length === 0
-                    ? <Text>Your cart is empty.</Text>
+                    ? <EmptyContent>Your cart is empty!</EmptyContent>
                     : collapseDuplicates(items).map((item, i) =>
-                        <ProductCard key={`cart-product-${i}`} product={item.product} cartQuantity={item.count} />,
+                        <ProductCard key={`cart-product-${i}`} product={item.product} />,
                     )
                 }
-                <Button h={"16"} rightIcon={<FaArrowRight />}>
+                <Button h={"16"} rightIcon={<FaArrowRight />} onClick={() => navigate(AppRoutes.Home)}>
                     Add more items
                 </Button>
             </VStack>
