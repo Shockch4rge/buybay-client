843833deea9dd2bca5ced18d3a89b5c2fa42c098
Favian <favouriteteo@gmail.com>
2023-02-02 01:14:58 +0800

    feat: fetch all products when 0 categories are selected

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/store/hooks";
 import {
     AspectRatio,
+    Box,
     Button,
     Card,
     CardBody,
@@ -18,12 +19,13 @@ import {
     Image,
     Stack,
     Text,
+    useCheckboxGroup,
     VStack,
 } from "@chakra-ui/react";
 import { SearchBar } from "./components/SearchBar";
 import { NavBar } from "../common/NavBar";
 import { Footer } from "../common/Footer";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import Utils from "../../util/Utils";
 import { AppRoutes } from "../../util/routes";
 
@@ -31,11 +33,10 @@ export const HomePage: React.FC = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const cart = useAppSelector(state => state.cart.items);
-    const { data: categories } = useGetAllCategoriesQuery({
-        limit: 25,
-    });
+    const { value: selectedCategories, getCheckboxProps } = useCheckboxGroup();
 
-    const [getCategoryProducts, { data: products }] = useLazyGetCategoryProductsQuery();
+    const { data: categories } = useGetAllCategoriesQuery({ limit: 25 });
+    const [getCategoryProducts, { data: products, isLoading }] = useLazyGetCategoryProductsQuery();
 
     const debounced = useCallback(
         Utils.debounce((categoryIds: string[]) => {
@@ -44,64 +45,78 @@ export const HomePage: React.FC = () => {
         [],
     );
 
+    useEffect(() => {
+        getCategoryProducts({ limit: 25, categoryIds: [] }).unwrap();
+    }, []);
+
     if (!categories) return <></>;
 
     return <>
         <NavBar />
         <Container maxW={"8xl"}>
             <SearchBar />
-            <Grid templateRows={"repeat(4, 1fr)"} templateColumns={"repeat(4, 1fr)"} gap={"8"}>
+            <Heading>
+                Returning products under {selectedCategories.length} {" "}
+                {selectedCategories.length === 1 ? "category" : "categories"}
+            </Heading>
+            <Grid mt={"6"} templateRows={"repeat(4, 1fr)"} templateColumns={"repeat(4, 1fr)"} gap={"8"}>
                 <GridItem p={"6"} rowSpan={4} colSpan={1} borderRadius={"8"} bg={"gray.50"}>
                     <Heading size={"md"}>Categories</Heading>
                     <VStack mt={"6"} align={"start"} spacing={"4"}>
                         <CheckboxGroup onChange={debounced}>
                             {categories.map(category =>
-                                <Checkbox key={category.id} colorScheme={"cyan"} size={"lg"} value={category.id}>
+                                <Checkbox {...getCheckboxProps()} key={category.id} colorScheme={"cyan"} size={"lg"} value={category.id}>
                                     {category.name}
                                 </Checkbox>,
                             )}
                         </CheckboxGroup>
                     </VStack>
                 </GridItem>
-                {products?.map((product, i) =>
-                    <GridItem key={`recommended-product-${product.id}-${i}`} rowSpan={1} colSpan={1}>
-                        <Card variant={"outline"}>
-                            <CardBody>
-                                <AspectRatio ratio={16/10}>
-                                    <Image
+                {isLoading
+                    ?
+                    <GridItem rowSpan={4} colSpan={3} display={"grid"} placeItems={"center"}>
+                        <Box>Loading products...</Box>
+                    </GridItem>
+                    :
+                    products?.map((product, i) =>
+                        <GridItem key={`recommended-product-${product.id}-${i}`} rowSpan={1} colSpan={1}>
+                            <Card variant={"outline"}>
+                                <CardBody>
+                                    <AspectRatio ratio={16/10}>
+                                        <Image
                                         // src={product.images.find(i => i.isThumbnail)!.url}
-                                        src={product.images[0]?.url ?? "https://i.scdn.co/image/ab6761610000e5eb45c0f559e90489af64359a59"}
-                                        alt='Green double couch with wooden legs'
-                                        borderRadius='lg'
-                                    />
-                                </AspectRatio>
-                                <Stack mt='6' spacing='3'>
-                                    <Heading size='md'>{product.name}</Heading>
-                                    <Text noOfLines={3}>
-                                        {product.description}
-                                    </Text>
-                                    <HStack spacing={"2"}>
-                                        <Text color='green.500' fontSize='2xl'>
-                                            ${product.price.toFixed(2)}
+                                            src={product.images[0]?.url ?? "https://i.scdn.co/image/ab6761610000e5eb45c0f559e90489af64359a59"}
+                                            alt='Green double couch with wooden legs'
+                                            borderRadius='lg'
+                                        />
+                                    </AspectRatio>
+                                    <Stack mt='6' spacing='3'>
+                                        <Heading size='md'>{product.name}</Heading>
+                                        <Text noOfLines={3}>
+                                            {product.description}
                                         </Text>
-                                        <Text>&bull; {product.quantity} left</Text>
-                                    </HStack>
-                                </Stack>
-                            </CardBody>
-                            <Divider />
-                            <CardFooter display={"flex"} justifyContent={"space-between"}>
-                                {cart.find(p => p.id === product.id) &&
+                                        <HStack spacing={"2"}>
+                                            <Text color='green.500' fontSize='2xl'>
+                                            ${product.price.toFixed(2)}
+                                            </Text>
+                                            <Text>&bull; {product.quantity} left</Text>
+                                        </HStack>
+                                    </Stack>
+                                </CardBody>
+                                <Divider />
+                                <CardFooter display={"flex"} justifyContent={"space-between"}>
+                                    {cart.find(p => p.id === product.id) &&
                                     <Button variant={"link"} onClick={() => navigate(AppRoutes.Cart)}>
                                         {cart.filter(p => p.id === product.id).length} in cart
                                     </Button>
-                                }
-                                <Button size={"sm"} variant={"primaryGhost"} onClick={() => navigate(AppRoutes.Product(product.id))}>
+                                    }
+                                    <Button size={"sm"} variant={"primaryGhost"} onClick={() => navigate(AppRoutes.Product(product.id))}>
                                         View Details
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    </GridItem>,
-                )}
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        </GridItem>,
+                    )}
             </Grid>
         </Container>
         <Footer />
