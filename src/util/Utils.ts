41a8089d437700c78d1eedbc97daa6173795a6bc
Favian <favouriteteo@gmail.com>
2023-02-10 07:47:45 +0800

    feat: added image compression helper

--- src/util/Utils.ts
+++ src/util/Utils.ts
@@ -7,6 +7,34 @@ const debounce = (fn: (...args: any[]) => any, delay : number) => {
     };
 };
 
+const compress = async (image: File) => {
+    if (image.type !== "image/jpeg" && image.type !== "image/jpg" && image.type !== "image/png") {
+        throw new Error("Image must be a JPEG or PNG");
+    }
+
+    const imageBitmap = await createImageBitmap(image);
+    const canvas = document.createElement("canvas");
+    canvas.width = imageBitmap.width;
+    canvas.height = imageBitmap.height;
+    const ctx = canvas.getContext("2d")!;
+    ctx.drawImage(imageBitmap, 0, 0);
+
+    // Turn into Blob
+    const blob = await new Promise<Blob>((res, rej) => {
+        canvas.toBlob(
+            blob => blob ? res(blob) : rej(new Error("Could not compress image")),
+            "image/jpeg",
+            0.5,
+        );
+    });
+
+    // Turn Blob into File
+    return new File([blob], image.name, {
+        type: blob.type,
+    });
+};
+
 export default {
     debounce,
+    compress,
 };
