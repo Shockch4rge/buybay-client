2d926e531b9c07ac02b35a200a1cef895322e460
Favian <favouriteteo@gmail.com>
2023-02-10 07:48:45 +0800

    refactor: updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,12 @@ import { SellProductPage } from "./pages/sell-product/SellProductPage";
 import { RegistrationPage } from "./pages/registration/RegistrationPage";
 import { AccountPage } from "./pages/account/AccountPage";
 import { EditProductPage } from "./pages/edit-product/EditProductPage";
+import { loadStripe } from "@stripe/stripe-js";
+import { CheckoutSuccessPage } from "./pages/checkout/CheckoutSuccessPage";
+import { CheckoutCancelledPage } from "./pages/checkout/CheckoutCancelledPage";
+import { OrderPage } from "./pages/order/OrderPage";
+
+const stripe = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const App: React.FC = () => {
     const { token } = useAuth();
@@ -33,6 +39,11 @@ const App: React.FC = () => {
             <Route path={AppRoutes.EditProduct(":id")} element={<EditProductPage />} />
             <Route path={AppRoutes.Registration} element={<RegistrationPage />} />
             <Route path={AppRoutes.Account} element={<AccountPage />} />
+            <Route path={AppRoutes.Checkout("")}>
+                <Route path={AppRoutes.Checkout("success")} element={<CheckoutSuccessPage />} />
+                <Route path={AppRoutes.Checkout("cancelled")} element={<CheckoutCancelledPage />} />
+            </Route>
+            <Route path={AppRoutes.Order()} element={<OrderPage />} />
         </Routes>
         <LoginModal />
         <ResetPasswordModal />
