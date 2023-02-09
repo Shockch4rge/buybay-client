9b07c41e97c1189fb268afcbca6d1f709f0fad1c
Favian <favouriteteo@gmail.com>
2023-02-10 07:49:11 +0800

    style: formatting

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -36,35 +36,49 @@
 //   }
 // }
 
-Cypress.Commands.add("login", (email = "john@gmail.com", password = "dddddddd") => {
+Cypress.Commands.add("login", (email = "john@gmail.com", password = "password123") => {
     cy.visit("/");
 
     cy.intercept({
         method: "POST",
         url: `/api/auth/login`,
-    }).as("loginResponse");
+    })
+        .as("login");
 
-    cy.contains("Login").click();
-    cy.get("input[name=email]").type(email);
-    cy.get("input[name=password]").type(password);
+    cy.contains("Login")
+        .click();
+    cy.get("input[name=email]")
+        .type(email);
+    cy.get("input[name=password]")
+        .type(password);
 
-    cy.get("button[type=submit]").contains("Login").click();
+    cy.get("button[type=submit]")
+        .contains("Login")
+        .click();
 
-    cy.wait("@loginResponse")
+    cy.wait("@login")
         .its("response.body")
         .should("have.any.keys", "token", "user");
 });
 
 Cypress.Commands.add("logout", () => {
-    cy.login("john@gmail.com", "dddddddd");
+    cy.login();
+    cy.intercept({
+        method: "POST",
+        url: `/api/auth/logout`,
+    }).as("logout");
     cy.get("button").contains("Account").click();
     cy.get("button").contains("Log out").click();
+
+    cy.wait("@logout")
+        .its("response.statusCode")
+        .should("eq", 200);
 
     cy.window()
         .its("store")
         .invoke("getState")
         .its("_auth")
-        .its("user")
+        .its("token")
         .should("be.null");
 });
 
