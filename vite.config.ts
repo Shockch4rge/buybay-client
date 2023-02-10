84951c28fb963168c7c45edd8f50c436d3e5c9cf
Favian <favouriteteo@gmail.com>
2023-02-10 12:33:44 +0800

    fix: detect vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 84,
-    host: true,
-  },
-  plugins: [react()],
-})
+    server: {
+        port: 84,
+        host: true,
+    },
+    plugins: [react()],
+});
